f8f84b8733f0cb5689ba852e4a294af0cb75c925
Axilotl <78441708+Axilotl17@users.noreply.github.com>
2025-11-25 21:32:09 -0500

    nothing works

--- index.js
+++ index.js
@@ -1,66 +1,95 @@
 const bgr = document.getElementById("background")
 const bgrctx = bgr.getContext('2d');
 
-resize()
-
-const mapSize = 20
-const mapLength = 10
-const mapGap = mapSize/10
-const spotGap = (bgr.width / (mapSize+(2*mapGap)))
+const mapSize = 60
+const mapLength = 100
+const nodeGap = bgr.width / mapSize
 
 var bgrMap = []
 
-for(j=0; j<mapLength; j++){
-    const noise = .2
-    row = []
-    for(i=0; i<mapSize; i++) {
-        spot = {
-            "color" : 0,
-            "i" : i,
-            "j" : j,
-            "x" : spotGap*(i+mapGap+0.5),
-            "y" : spotGap*(j+mapGap),
-            "border" : {}
+var colors = [
+    "rgb(122, 90, 58)",
+    "rgba(132, 100, 72, 1)",
+    "rgb(104, 77, 54)",
+    "rgba(120, 97, 70, 1)",
+]
+
+populateNodes(bgrMap, 40)
+genBackground()
+resize()
+
+var scrollPos = 0
+//var topNode = 
+
+gameLoop()
+
+function populateNodes(map){
+    for(j=0; j<mapLength; j++){
+        const noise = .3
+        row = []
+        for(i=0; i<mapSize; i++) {
+            node = {
+                "color" : 0,
+                "i" : i,
+                "j" : j,
+                "x" : nodeGap*i,
+                "y" : nodeGap*j,
+                "border" : {}
+            }
+
+            if(j>0){ // checking for existing border northward
+                node.border.nx = nodeGap - map[j-1][i].border.sx;
+                node.border.ny = map[j-1][i].border.sy;
+            } else {
+                node.border.nx = (Math.random() - 0.5) * (noise * nodeGap);
+                node.border.ny = (-0.5 * nodeGap) + (Math.random() - 0.5) * (noise * nodeGap);
+            }
+
+            node.border.ex = (0.5 * nodeGap) + (Math.random() - 0.5) * (noise * nodeGap);
+            node.border.ey = (Math.random() - 0.5) * (noise * nodeGap);
+
+            node.border.sx = (Math.random() - 0.5) * (noise * nodeGap);
+            node.border.sy = (0.5 * nodeGap) + (Math.random() - 0.5) * (noise * nodeGap);
+
+            if(i>0){ // checking for existing border westward
+                node.border.wx = row[i-1].border.ex;
+                node.border.wy = nodeGap - row[i-1].border.ey;
+            } else {
+                node.border.wx = (-0.5 * nodeGap) + (Math.random() - 0.5) * (noise * nodeGap);
+                node.border.wy = (Math.random() - 0.5) * (noise * nodeGap);
+            }
+
+            row.push(node)
         }
-
-        if(j>0){ // checking for existing border northward
-            spot.border.nx = bgrMap[j-1][i].border.sx;
-            spot.border.ny = bgrMap[j-1][i].border.sy;
-        } else {
-            spot.border.nx = spot.x + (Math.random() - 0.5) * (noise * spotGap);
-            spot.border.ny = spot.y - (0.5 * spotGap) + (Math.random() - 0.5) * (noise * spotGap);
-        }
-
-        spot.border.ex = spot.x + (0.5 * spotGap) + (Math.random() - 0.5) * (noise * spotGap);
-        spot.border.ey = spot.y + (Math.random() - 0.5) * (noise * spotGap);
-
-        spot.border.sx = spot.x + (Math.random() - 0.5) * (noise * spotGap);
-        spot.border.sy = spot.y + (0.5 * spotGap) + (Math.random() - 0.5) * (noise * spotGap);
-
-        if(i>0){ // checking for existing border westward
-            spot.border.wx = row[i-1].border.ex;
-            spot.border.wy = row[i-1].border.ey;
-        } else {
-            spot.border.wx = spot.x - (0.5 * spotGap) + (Math.random() - 0.5) * (noise * spotGap);
-            spot.border.wy = spot.y + (Math.random() - 0.5) * (noise * spotGap);
-        }
-
-        row.push(spot)
+        map.push(row)
     }
-    bgrMap.push(row)
 }
 
-const li = Math.floor(mapSize / 4);
-const ri = Math.floor((3 * mapSize) / 4);
-const cj = Math.floor(mapLength / 2);
+function genBackground() {
+    const bgrGroundTop = 30
+    // for(n = bgrGroundTop; n<mapLength; n+=2) {
+    //     genCluster(bgrMap, rand(0, mapSize), n, 3, 2, 1)
+    // }
+    // for(n = bgrGroundTop; n<mapLength; n+=2) {
+    //     genCluster(bgrMap, rand(0, mapSize), n, 2, 3, 2)
+    // }
+    // for(n = bgrGroundTop; n<mapLength; n+=4) {
+    //     genCluster(bgrMap, rand(0, mapSize), n, 1, 1, 3)
+    // }
+    genCluster(bgrMap, mapSize/2, 30, 3, 3, 1)
+}  
 
-function bgrNoise(si, sj) {
+/**
+ * 
+ * @param {object} map 
+ * @param {number} si - starting position i
+ * @param {number} sj - starting position j 
+ * @param {number} r - radius
+ * @param {number} noise 
+ * @param {number} color 
+ */
 
-    const radius = 3
-    const noise = 3
-
-    // Local window bounds (+/- 4 from center)
-    const r = Math.ceil(radius + noise);
+function genCluster(map, si, sj, r, noise, color) {
     for (let j = sj - r; j <= sj + r; j++) {
         for (let i = si - r; i <= si + r; i++) {
 
@@ -72,50 +101,48 @@ function bgrNoise(si, sj) {
             const dj = j - sj;
             const dist = Math.sqrt(di * di + dj * dj);
 
-            const threshold = radius + (Math.random() - 0.5)*noise;
-            if (dist < threshold) bgrMap[j][i].color = 1;
+            const threshold = r + (Math.random() - 0.5)*noise;
+            if (dist < threshold) map[j][i]["color"] = color;
         }
     }
 }
 
-bgrNoise(li, cj);
-bgrNoise(ri, cj);
 
-function drawMap(){
-    getAllClusters(bgrMap).forEach(cluster => {
-        cluster.forEach(([i, j]) => {
-            // i = 6
-            // j = 7
-            points = getAreaPts(bgrMap, i, j)
-            bgrctx.beginPath();
-            bgrctx.moveTo(points[0][0], points[0][1]);
-            points.forEach(([x, y]) => {
-                bgrctx.lineTo(x, y);
-            })
-
-            bgrctx.closePath();
-
-            bgrctx.fillStyle = "rgba(0, 200, 0, 1)";
-            bgrctx.strokeStyle = "black";
-            bgrctx.lineWidth = -1;
-
-            bgrctx.fill();
-        })
-    })
-        
+function drawMap(map){   
     runForAll((i, j) => {
-        let spot = bgrMap[j][i]
-        drawSpot(spot);
+        let node = map[j][i]
+
+        if(node.color===0) return;
+        // i = 6
+        // j = 7
+        points = getAreaPts(map, i, j)
+        bgrctx.beginPath();
+        bgrctx.moveTo(points[0][0], points[0][1]);
+        points.forEach(([x, y]) => {
+            bgrctx.lineTo(x, y);
+        })
+
+        bgrctx.closePath();
+
+        bgrctx.fillStyle = colors[node.color];
+        bgrctx.fill();
+        //drawNode(node);
     })
 }
 
+function drawBackground() {
+    bgrctx.fillStyle = colors[0]
+    bgrctx.fillRect(0, Math.min(bgr.height/2 - scrollPos), bgr.width, bgr.height)
+}
+
+// unused
 function getAllClusters(map) {
     const visited = map.map(row => row.map(() => false));
     const clusters = [];
 
     runForAll((si, sj) => {
         // Only start a cluster if this cell is an unvisited 1
-        if (visited[sj][si] || map[sj][si].color !== 1) return;
+        if (visited[sj][si] || map[sj][si].color === 0) return;
 
         const stack = [[si, sj]];
         const cluster = [];
@@ -146,45 +173,72 @@ function getAllClusters(map) {
 
 function getAreaPts(map, i, j){
     const points = []
-    spot = map[j][i]
-    adjSpots = getAdjSpots(i, j)
+    node = map[j][i]
+    adjNodes = getAdjNodes(map, i, j)
 
     // cache neighbors + colors + borders for speed
-    const nw = adjSpots.nw, n = adjSpots.n, ne = adjSpots.ne, e = adjSpots.e;
-    const se = adjSpots.se, s = adjSpots.s, sw = adjSpots.sw, w = adjSpots.w;
+    const nw = adjNodes.nw, n = adjNodes.n, ne = adjNodes.ne, e = adjNodes.e;
+    const se = adjNodes.se, s = adjNodes.s, sw = adjNodes.sw, w = adjNodes.w;
 
-    const sc = spot.color;
+    const nc = node.color;
 
-    const cNW = nw && nw.color === sc;
-    const cN  = n  && n.color  === sc;
-    const cNE = ne && ne.color === sc;
-    const cE  = e  && e.color  === sc;
-    const cSE = se && se.color === sc;
-    const cS  = s  && s.color  === sc;
-    const cSW = sw && sw.color === sc;
-    const cW  = w  && w.color  === sc;
+    const cNW = adjNodes.nw && adjNodes.nw.color === nc;
+    const cN  = n  && n.color  === nc;
+    const cNE = ne && ne.color === nc;
+    const cE  = e  && e.color  === nc;
+    const cSE = se && se.color === nc;
+    const cS  = s  && s.color  === nc;
+    const cSW = sw && sw.color === nc;
+    const cW  = w  && w.color  === nc;
 
-    const sb = spot.border;
+    const nb = node.border
+
+    const sb = {
+        "nx" : (node.i * nodeGap) + nb.nx,
+        "ny" : (node.j * nodeGap) + nb.ny,
+        "ex" : (node.i * nodeGap) + nb.ex,
+        "ey" : (node.j * nodeGap) + nb.ey,
+        "sx" : (node.i * nodeGap) + nb.sx,
+        "sy" : (node.j * nodeGap) + nb.sy,
+        "wx" : (node.i * nodeGap) + nb.wx,
+        "wy" : (node.j * nodeGap) + nb.wy,
+    };
+
+    const nw
 
     /* 
     the following code is an amalgamation of ifs and elses that somehow formulates
     coherent code. it is not understandable, it barely works, and it is slow.  
     */
 
+    if(Object.keys(adjNodes).filter(key => adjNodes[key].color === nc).length === 8){
+        points.push(
+            [n.border.wx, n.border.wy],
+            [n.border.ex, n.border.ey],
+            [e.border.nx, e.border.ny],
+            [e.border.sx, e.border.sy],
+            [s.border.ex, s.border.ey],
+            [s.border.wx, s.border.wy],
+            [w.border.sx, w.border.sy],
+            [w.border.nx, w.border.ny]
+        );
+        return points;
+    }
+
     if (!cNW) {
         if (!cW && cSW) points.push([sb.wx, sb.wy]);
         if (!cN) points.push([sb.nx, sb.ny]);
     } else {
         const b = nw.border;
-        if (!cW) points.push([b.sx, b.sy]);
-        points.push([b.ex, b.ey]);
+        if (!cW) points.push([(nw.i * nodeGap) + b.sx, b.sy]);
+        points.push([(nw.i * nodeGap) + b.ex, (nw.j * nodeGap) + b.ey]);
         if (cNE && !cN) points.push([sb.nx, sb.ny]);
     }
 
     if (cN) {
         const b = n.border;
-        if (!cNW) points.push([b.wx, b.wy]);
-        points.push([b.ex, b.ey]);
+        if (!cNW) points.push([(n.i * nodeGap) + b.wx, b.wy]);
+        points.push([(n.i * nodeGap) + b.ex, b.ey]);
     }
 
     if (!cNE) {
@@ -238,41 +292,25 @@ function getAreaPts(map, i, j){
     return points;
 }
 
-
-function drawSpot(spot){
-    bgrctx.beginPath();
-    bgrctx.arc(spot.x, spot.y, (spot.color+1)*(spotGap/8), 0, 6.2832);
-    bgrctx.stroke();
-}
-
-function resize() {
-    const dpr = window.devicePixelRatio || 1;
-    bgr.style.width = window.innerWidth + "px";
-    bgr.style.height = window.innerHeight + "px";
-
-    bgr.width = window.innerWidth * dpr;
-    bgr.height = window.innerHeight * dpr;
-}
-
-function getAdjSpots(i, j) {
-    adjSpots = {
-        "nw" : bgrMap[j - 1]?.[i - 1],
-        "n"  : bgrMap[j - 1]?.[i],
-        "ne" : bgrMap[j - 1]?.[i + 1],
-        "w"  : bgrMap[j]?.[i - 1],
-        "e"  : bgrMap[j]?.[i + 1],
-        "sw" : bgrMap[j + 1]?.[i - 1],
-        "s"  : bgrMap[j + 1]?.[i],
-        "se" : bgrMap[j + 1]?.[i + 1]
+function getAdjNodes(map, i, j) {
+    adjNodes = {
+        "nw" : map[j - 1]?.[i - 1],
+        "n"  : map[j - 1]?.[i],
+        "ne" : map[j - 1]?.[i + 1],
+        "w"  : map[j]?.[i - 1],
+        "e"  : map[j]?.[i + 1],
+        "sw" : map[j + 1]?.[i - 1],
+        "s"  : map[j + 1]?.[i],
+        "se" : map[j + 1]?.[i + 1]
     }
 
-    for (const direction in adjSpots) { // purge all nonexisting spots
-        if (adjSpots[direction] === undefined) {
-            delete adjSpots[direction];
+    for (const direction in adjNodes) { // purge all nonexisting nodes
+        if (adjNodes[direction] === undefined) {
+            delete adjNodes[direction];
         }
     }
 
-    return adjSpots
+    return adjNodes
 }
 
 function runForAll(func) {
@@ -291,14 +329,49 @@ function runForAdjacent(func, io, jo) {
     }
 }
 
-// runForAll((x, y) => {
-    
-// })
+function rand(min, max) {
+    return Math.floor(Math.random()*(max - min + 1)) + min
+}
 
 function gameLoop(now) {
+    let w = bgr.width
+    let h = bgr.height
+    bgrctx.clearRect(0, 0, w, h);
+
+    drawBackground()
+    drawMap(bgrMap)
     requestAnimationFrame(gameLoop);
+}
+
+function resize() {
+
+    const dpr = window.devicePixelRatio || 1;
+    const d = 3
+    const s = Math.round(Math.pow(10,d) * window.innerWidth * dpr / bgr.width) / Math.pow(10,d)
+    console.log(s)
+
+    runForAll((i, j) => {
+        node = bgrMap[j][i]
+        node.x *= s
+        node.y *= s
+
+        node.border.nx *= s
+        node.border.ny *= s
+        node.border.ex *= s
+        node.border.ey *= s
+        node.border.sx *= s
+        node.border.sy *= s
+        node.border.wx *= s
+        node.border.wy *= s
+        
+    })
+
+    bgr.style.width = window.innerWidth + "px";
+    bgr.style.height = window.innerHeight + "px";
+
+    bgr.width = window.innerWidth * dpr;
+    bgr.height = window.innerHeight * dpr;
 }
 
 window.addEventListener('resize', resize);
 
-drawMap()
